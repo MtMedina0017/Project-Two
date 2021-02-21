e177ccc009e962365e77e8c4e518a5552f2bd842
teresa luna <tmluna_18@outlook.com>
2021-02-21 13:40:02 -0600

    adding code

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,115 +1,204 @@
-d3.csv('Data/merged_WO_breweries.csv').then(function(beerData, err) {
-    if (err) throw err;
-    console.log(beerData);
+var svgWidth = 500;
+var svgHeight = 300;
 
-    //parse data
-    beerData.forEach(function(data) {
-        data.average_num_of_drinks = +data.average_num_of_drinks;
-        data.cost_per_capita = +data.cost_per_capita;
-        data.cost_per_drink = +data.cost_per_drink;
-        data.percentage = +data.percentage;
-        data.total_cost = +data.total_cost;
-    });
+var margin = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10
+};
 
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
+var svg = d3
+    .select(".bubble")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-function initialize() {
-    //**initial start up populate Test Subject ID No */
-    
-    var selectState = d3.select("#state")
-    var states = beerData.state
-    states.forEach(state => {
-        selectState.append("option")
-            .property("value", state).text(state)
-    });
-
-    var svgWidth = 960;
-    var svgHeight = 500;
-
-    var margin = {
-    top: 20,
-    right: 40,
-    bottom: 80,
-    left: 100
-    };
-
-    var width = svgWidth - margin.left - margin.right;
-    var height = svgHeight - margin.top - margin.bottom;
-
-    var svg = d3
-        .select(".bubble")
-        .append("svg")
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
-
-    var chartGroup = svg.append("g")
+var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    var chosenXAxis = "average_num_of_drinks";
-    function xScale(beerData, chosenXAxis) {
-        // create scales
-        var xLinearScale = d3.scaleLinear()
-          .domain([d3.min(beerData, d => d[chosenXAxis]) * 0.8,
-            d3.max(beerData, d => d[chosenXAxis]) * 0.8
-          ])
-          .range([0, width]);
-      
-        return xLinearScale;
-      
-    }
-    function renderAxes(newXScale, xAxis) {
-        var bottomAxis = d3.axisBottom(newXScale);
-      
-        xAxis.transition()
-          .duration(1000)
-          .call(bottomAxis);
-      
-        return xAxis;
-    }
-    function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+var chosenXAxis = "average_num_of_drinks";
+function xScale(alcoholData, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(alcoholData, d => d[chosenXAxis]) * 0.8,
+        d3.max(alcoholData, d => d[chosenXAxis]) * 0.8
+        ])
+        .range([0, width]);
 
-        circlesGroup.transition()
-          .duration(1000)
-          .attr("cx", d => newXScale(d[chosenXAxis]));
-      
-        return circlesGroup;
-    }
-    function updateToolTip(chosenXAxis, circlesGroup) {
+    return xLinearScale;
+
+}
+function renderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAxis);
+
+    return xAxis;
+}
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenXAxis]));
+
+    return circlesGroup;
+}
+
+function updateToolTip(chosenXAxis, circlesGroup) {
 
     var label;
-    
+
     if (chosenXAxis === "average_num_of_drinks") {
         label = "Intensity:";
     }
     else {
         label = "Prevalence:";
     }
-    
+
     var toolTip = d3.tip()
         .attr("class", "tooltip")
         .offset([80, -60])
-        .html(function(d) {
-        return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
+        .html(function (d) {
+            return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
         });
-    
+
     circlesGroup.call(toolTip);
-    
-    circlesGroup.on("mouseover", function(data) {
+
+    circlesGroup.on("mouseover", function (data) {
         toolTip.show(data);
     })
         // onmouseout event
-        .on("mouseout", function(data, index) {
-        toolTip.hide(data);
+        .on("mouseout", function (data, index) {
+            toolTip.hide(data);
         });
-    
-    return circlesGroup;
-    }
-    var xLinearScale = xScale(beerData, chosenXAxis);
-    var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(beerData, d => d.num_hits)])
-    .range([height, 0]);
 
+    return circlesGroup;
 }
-function optionChanged() {
-    initialize()
-}
+
+d3.csv('Data/merged_data.csv').then(function (alcoholData, err) {
+    if (err) throw err;
+    console.log(alcoholData);
+
+    //parse data
+    alcoholData.forEach(function (data) {
+        data.average_num_of_drinks = +data.average_num_of_drinks;
+        data.percentage = +data.percentage;
+        data.name = +data.name;
+    });
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(alcoholData, chosenXAxis);
+
+    // Create y scale function
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(alcoholData, d => d.name)])
+        .range([height, 0]);
+
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(alcoholData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cy", d => yLinearScale(d.name))
+        .attr("r", 20)
+        .attr("fill", "pink")
+        .attr("opacity", ".5");
+
+    // Create group for two x-axis labels
+    var labelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    var intensityLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("value", "average_num_of_drinks") // value to grab for event listener
+        .classed("active", true)
+        .text("Average # of drinks");
+
+
+    var prevalenceLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("value", "percentage") // value to grab for event listener
+        .classed("inactive", true)
+        .text("Prevalence Percentage of Bringe Drinking");
+
+    // append y axis
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .classed("axis-text", true)
+        .text("Number of Breweries per State");
+
+    // updateToolTip function above csv import
+    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+    // x axis labels event listener
+    labelsGroup.selectAll("text")
+        .on("click", function () {
+            // get value of selection
+            var value = d3.select(this).attr("value");
+            if (value !== chosenXAxis) {
+
+                // replaces chosenXAxis with value
+                chosenXAxis = value;
+
+                // console.log(chosenXAxis)
+
+                // functions here found above csv import
+                // updates x scale for new data
+                xLinearScale = xScale(alcoholData, chosenXAxis);
+
+                // updates x axis with transition
+                xAxis = renderAxes(xLinearScale, xAxis);
+
+                // updates circles with new x values
+                circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+                // updates tooltips with new info
+                circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+                // changes classes to change bold text
+                if (chosenXAxis === "average_num_of_drinks") {
+                    intensityLabel
+                        .classed("active", true)
+                        .classed("inactive", false);
+                    prevalenceLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                }
+                else {
+                    intensityLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                    prevalenceLabel
+                        .classed("active", true)
+                        .classed("inactive", false);
+                }
+            }
+        });
+}).catch(function (error) {
+    console.log(error);
+});
